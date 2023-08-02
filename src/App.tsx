c0bfa21b4ece28635518256b354a01cd5b4de3a6
HJRYU\o__hy <o__hyeah@naver.com>
2023-08-02 14:10:03 +0900

    feat: Globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import { styled } from "styled-components";
 import Router from "./Router";
+import Globalstyle from "./styles/Globalstyle";
 
 function App() {
   return (
     <>
+      <Globalstyle />
       <Router />
     </>
   );
