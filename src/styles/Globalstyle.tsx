9ce31686a208a2a5a2cf55d881ad50c72d9cbdb3
HJRYU\o__hy <o__hyeah@naver.com>
2023-08-02 15:38:39 +0900

    style: Add font (Sans)

--- src/styles/Globalstyle.tsx
+++ src/styles/Globalstyle.tsx
@@ -1,6 +1,7 @@
-import { styled, createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 const Globalstyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -48,7 +49,11 @@ table {
   border-collapse: collapse;
   border-spacing: 0;
 }
-body{
+body{  
+  font-weight: 300;
+  font-family: 'Source Sans Pro', sans-serif;
+  color:black;
+  line-height: 1.2;
 }
 a{
   text-decoration: none;
